cd2182c2b8c53bc2abd3b95a44cc99f3d1ef8815
Titiksha <titiksha5248@gmail.com>
2025-03-09 17:04:55 +0530

    unused variable

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,8 @@ import {
 import { FaLinkedinIn } from "react-icons/fa";
 import instaIMG from "../Assets/insta.png"
 function Footer() {
-  let date = new Date();
-  let year = date.getFullYear();
+  
+ 
   return (
     <Container fluid className="footer">
       <Row>
